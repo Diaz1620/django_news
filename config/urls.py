aaa4b2cafcad0e761e757331dbb788e6fb8b9fd4
Diaz1620 <77040700+Diaz1620@users.noreply.github.com>
2021-11-04 09:48:09 -0400

    added signup/login page

--- config/urls.py
+++ config/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.urls.conf import include
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('signup/', include('a'))
+    path('accounts/', include('accounts.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+
 ]
